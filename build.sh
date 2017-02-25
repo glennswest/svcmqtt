5e8439da2fd2a5f6fae24c5d9a53397e92044140
Glenn West <gwest@redhat.com>
2017-02-25 13:18:46 +0800

    Touchup readme

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ yum -y install -y avahi-compat-libdns_sd-devel.x86_64
 yum -y groupinstall development
 docker build -t svcmqtt .
 docker tag svcmqtt s001.site.com:5000/svcmqtt
+docker tag svcmqtt svcmqtt.site.com
 docker push s001.site.com:5000/svcmqtt
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+Building on Openshift
+oc new-app https://github.com/glennswest/svcmqtt --strategy=docker
+
