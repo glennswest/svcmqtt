ea105bb8d71c7be988b7fb7f163d3acb1c8b9cc9
Glenn West <glennswest@neuralcloudcomputing.com>
2017-05-14 06:31:28 +0000

    Add systemd support

---
+++ install-service.sh
@@ -0,0 +1,3 @@
+cp svcmqtt.service /etc/systemd/system/svcmqtt.service
+systemctl enable svcmqtt.service
+systemctl start svcmqtt.service
--- runncc.sh
+++ runncc.sh
@@ -1,3 +1,3 @@
 docker kill svcmqtt.ncc9.com
 docker rm svcmqtt.ncc9.com
-docker run -d -p 80 --net=host --name svcmqtt.ncc9.com svcmqtt
+docker run -d -p 80 -p 1883 --net=host --name svcmqtt.ncc9.com svcmqtt
