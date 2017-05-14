9c55e68431eeefecd56c1134908911eb41f68142
glennswest <glennswest@neuralcloudcomputing.com>
2017-05-14 11:15:42 +0000

    Fixup build and install

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export PYTHON=/usr/bin/python
 yum -y install -y avahi-compat-libdns_sd-devel.x86_64
 yum -y groupinstall development
 docker build -t svcmqtt .
-#docker tag svcmqtt s001.site.com:5000/svcmqtt
-#docker tag svcmqtt svcmqtt.site.com
-#docker push s001.site.com:5000/svcmqtt
+docker tag svcmqtt ctl.ncc9.com:5000/svcmqtt
+docker tag svcmqtt svcmqtt.site.com
+docker push ctl.ncc9.com:5000/svcmqtt
--- install-service.sh
+++ install-service.sh
@@ -1,3 +1,4 @@
 cp svcmqtt.service /etc/systemd/system/svcmqtt.service
+systemctl disable svcmqtt.service
 systemctl enable svcmqtt.service
 systemctl start svcmqtt.service
