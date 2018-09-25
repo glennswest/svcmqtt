ab12fe6cd27cec15c8552623030b94a49d50c103
gwest <gwest@redhat.com>
2018-09-25 04:30:49 -0500

    Fix naming

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export PYTHON=/usr/bin/python
 yum -y install -y avahi-compat-libdns_sd-devel.x86_64
 yum -y groupinstall development
 docker build -t svcmqtt .
-docker tag svcmqtt ctl.ncc9.com:5000/svcmqtt
-docker tag svcmqtt svcmqtt.site.com
-docker push ctl.ncc9.com:5000/svcmqtt
+docker tag svcmqtt ${SVCHOST}:5000/svcmqtt
+docker tag svcmqtt svcmqtt.${SVCDOMAIN}
+docker push ${SVCHOST}:5000/svcmqtt
